10b4b341fde776dab3df829dfd83367eaca35643
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2023-11-25 21:40:21 -0500

    e

---
+++ HighResolutionSupport.cs
@@ -0,0 +1,133 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jellyfin.Plugin.ESRGAN
+{
+    /// <summary>
+    /// The main plugin.
+    /// </summary>
+    public class HighResolutionSupportPlugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public override string Name => "High Resolution Support";
+        public override Guid Id => Guid.Parse("70a8d2e2-6364-42a2-91fd-46ec8a6b9c45");
+
+        private readonly ILibraryManager _libraryManager;
+
+        public HighResolutionSupportPlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, ILibraryManager libraryManager)
+            : base(applicationPaths, xmlSerializer)
+        {
+            _libraryManager = libraryManager;
+            InitializePlugin();
+        }
+
+        private void InitializePlugin()
+        {
+            var enhancer = new HighResolutionEnhancer(_libraryManager);
+            enhancer.AddHighResolutionOptions();
+        }
+
+        public override void UpdateConfiguration(BasePluginConfiguration configuration)
+        {
+            base.UpdateConfiguration(configuration);
+        }
+
+        // Implement IHasWebPages interface if you plan to have a configuration page
+        public PluginPageInfo[] GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "HighResolutionSupport",
+                    EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
+                }
+            };
+        }
+
+        IEnumerable<PluginPageInfo> IHasWebPages.GetPages()
+        {
+            return this.GetPages();
+        }
+    }
+
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        // Add any specific configuration options here
+    }
+
+    public class HighResolutionEnhancer
+    {
+        private readonly ILibraryManager _libraryManager;
+
+        public HighResolutionEnhancer(ILibraryManager libraryManager)
+        {
+            _libraryManager = libraryManager;
+        }
+
+        public void AddHighResolutionOptions()
+        {
+            // Placeholder for server availability check
+            bool isServerAvailable = CheckServerAvailability(); // Implement this method
+
+            if (isServerAvailable)
+            {
+                var allMediaItems = _libraryManager.GetItemList(new InternalItemsQuery());
+                foreach (var item in allMediaItems)
+                {
+                    if (item is Video videoItem) // Check if the item is a Video
+                    {
+                        // Add or modify the 4K resolution option
+                        AddOrModify4KResolution(videoItem);
+                    }
+                }
+            }
+        }
+
+        private void AddOrModify4KResolution(Video videoItem)
+        {
+            var mediaStreams = videoItem.GetMediaStreams();
+
+            var token = new CancellationToken();
+
+            // Check if the video already has a 4K version
+            var is4K = mediaStreams.Any(ms => ms.Width >= 3840 && ms.Height >= 2160);
+
+            if (!is4K)
+            {
+                // Logic to create a 4K version of the video
+                // Note: This is a conceptual implementation. Actual implementation will depend on how Jellyfin handles media streams and transcoding
+                var newMediaStream = new MediaStream
+                {
+                    Width = 3840,
+                    Height = 2160,
+                    BitRate = 120000000,
+                    AverageFrameRate = 60,
+                    // Other necessary properties like codec, bitrate, etc.
+                };
+
+                mediaStreams.Add(newMediaStream);
+
+                // Update the item in the library
+                _libraryManager.UpdateItemAsync(videoItem, videoItem.GetParent(), ItemUpdateType.MetadataEdit, token);
+            }
+        }
+
+        private bool CheckServerAvailability()
+        {
+            // Implement server availability check here
+            return true;
+        }
+    }
+}
